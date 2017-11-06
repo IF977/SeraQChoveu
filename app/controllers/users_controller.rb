b99808f50ae63deb2c2d16f397bc01bced916d94
Samir Ferreira <svaf2@cin.ufpe.br>
2017-11-06 20:36:17 -0300

    Feature login #14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         flash[:success] = "Cadastro realizado com sucesso!"
         format.html { redirect_to @user }
         format.json { render :show, status: :created, location: @user }

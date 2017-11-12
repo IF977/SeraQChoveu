4ac5cb6a70a917a6767f1ee0e10d527300177710
Samir Ferreira <svaf2@cin.ufpe.br>
2017-11-11 22:17:17 -0300

    Remember me feature #36

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,48 @@
 module SessionsHelper
+  # Faz o login do usuario
   def log_in(user)
       session[:user_id] = user.id
   end
 
-  def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
+  # Salva o usuario em uma sessao persistente
+  def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Retorna o usuario logado, se existir
+  # Se nao estiver logado, loga e retorna o usuario correspondente ao cookie remember_token
+  def current_user
+      if (user_id = session[:user_id])
+          @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+          user = User.find_by(id: user_id)
+          if user && user.authenticated?(cookies[:remember_token])
+              log_in user
+              @current_user = user
+          end
+      end
+  end
+
+  # Retorna true se existe usuario logado, false caso contrario
   def logado?
       !current_user.nil?
   end
 
+  # Esquece uma sessao persistente
+  def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+  end
+
+  # Faz o logoff do usuario
   def log_out
+      forget(current_user)
       session.delete(:user_id)
       @current_user = nil
       flash[:danger] = 'Logoff realizado!'
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,39 @@
 class User < ApplicationRecord
     has_secure_password
+    attr_accessor :remember_token
     before_save {email.downcase!}
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
     validates :password, presence: true, length: {minimum: 6}
+
+    # Retorna um hash criptografado da string passada como parametro
+    def self.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Gera um novo token aleatorio
+    def self.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Salva um usuario na base de dados para uso em sessoes persistentes
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Retorna true se o token passado como parametro bate com a hash criptografada
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    # Esquece um usuario, ou seja, desfaz o metodo remember
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end

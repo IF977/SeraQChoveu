28e58cbdcead192ef0e0bffef4bc09e5990471f8
Samir Ferreira <svaf2@cin.ufpe.br>
2017-11-12 14:36:34 -0300

    Configurando Sendgrid #29

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
-      :port           => '465',
+      :port           => '587',
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],

e546e3c19bd8436b668fb3f603d3d66a18807fcf
Samir Ferreira <svaf2@cin.ufpe.br>
2017-11-13 00:00:03 -0300

    Fix bug mailer #29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     attr_accessor :remember_token, :reset_token
-    before_save {email.downcase!}
+    before_save :downcase_email
+    before_create :create_activation_digest
+    
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},

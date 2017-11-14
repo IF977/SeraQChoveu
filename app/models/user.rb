859746ebd03284963575cb4f0af4c3612d209321
Samir Ferreira <svaf2@cin.ufpe.br>
2017-11-14 17:25:28 -0300

    Fix Cadastro

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     attr_accessor :remember_token, :reset_token
     before_save :downcase_email
-    before_create :create_activation_digest
-    
+
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},
@@ -62,10 +61,5 @@ class User < ApplicationRecord
     # Converte email para lower-case.
     def downcase_email
       self.email = email.downcase
-    end
-
-    def create_activation_digest
-        self.activation_token  = User.new_token
-        self.activation_digest = User.digest(activation_token)
     end
 end
